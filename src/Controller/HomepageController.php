b8f3095571cc83e2bf841e67ee10188a38fce5aa
JosephLindzius <joseph.lindzius@gmail.com>
2019-10-21 16:32:34 +0200

    trying symfony for the first time

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -1,19 +1,23 @@
 <?php
-
+//name space is a group of classes give differernt categories.  if you give specific name....
 namespace App\Controller;
 
+// use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
-{
+{ // this comment is an annottation... comments that do stuff. reflection/after all is loaded then it will be over it running...inject behavior that would
     /**
      * @Route("/", name="homepage")
      */
     public function index()
     {
+       // $user = new User();
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'name' => 'Joe',
         ]);
     }
 }
