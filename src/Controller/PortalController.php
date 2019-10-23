806c58b90d53fbe1d6df05a95521fd714ead2af3
janvdv96 <janvandevelde.dev@gmail.com>
2019-10-23 17:17:21 +0200

    added language filter to modules screen

--- src/Controller/PortalController.php
+++ src/Controller/PortalController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Language;
+use App\Entity\LearningModule;
+use App\Entity\LearningModuleTranslation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PortalController extends AbstractController
 {
     /**
-     * @Route("/portal", name="portal")
+     * @Route("/portal", name="app_portal")
      */
     public function index()
     {
+        $language = $this->getDoctrine()->getRepository(Language::class)->find($_GET['lang']);
+        $modules = $this->getDoctrine()->getRepository(LearningModule::class)->findBy(['isPublished' => true]);
         return $this->render('portal/index.html.twig', [
             'controller_name' => 'PortalController',
+            'language' => $language,
+            'modules' => $modules,
         ]);
     }
 }
