20879842ab729810dd2af591b56c82c11a9d7a3f
bona-kim <bona.kim@outlook.be>
2019-10-24 10:51:09 +0200

    fix the migration errors

--- src/Migrations/Version20191023141931.php
+++ src/Migrations/Version20191023141931.php
@@ -22,9 +22,8 @@ final class Version20191023141931 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE product');
         $this->addSql('ALTER TABLE language ADD code VARCHAR(2) NOT NULL');
-        $this->addSql('ALTER TABLE user ADD language_id INT NOT NULL, ADD password_hash VARCHAR(255) NOT NULL, ADD is_partner TINYINT(1) NOT NULL, ADD badgr_key VARCHAR(255) NOT NULL, ADD username VARCHAR(255) NOT NULL, ADD avatar VARCHAR(255) DEFAULT NULL, ADD created DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user ADD language_id INT NOT NULL, ADD password_hash VARCHAR(255) NOT NULL, ADD is_partner TINYINT(1) NOT NULL, ADD badgr_key VARCHAR(255) NOT NULL, ADD username VARCHAR(255) NOT NULL, ADD avatar VARCHAR(255) DEFAULT NULL, ADD created DATETIME NOT NULL DEFAULT GETDATE()');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64982F1BAF4 FOREIGN KEY (language_id) REFERENCES language (id)');
         $this->addSql('CREATE INDEX IDX_8D93D64982F1BAF4 ON user (language_id)');
     }
