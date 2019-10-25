2760c29ba2d105fc28fdfbdb1f25a65f489d05a4
Lisonallie <allisonvanlinden@gmail.com>
2019-10-25 14:32:29 +0200

    add working iPlayer and try to find whole link in input

--- src/Controller/MarkdownToHTMLController.php
+++ src/Controller/MarkdownToHTMLController.php
@@ -18,12 +18,18 @@ class MarkdownToHTMLController extends AbstractController
 
         $form->handleRequest($request);
         $user_markdown = $form->getData();
-
+        
         if($form->isSubmitted() && $form->isValid()) {
             $user_markdown;
             if(preg_grep("/(?:https?:\/\/)?www\.(?:youtube\.com|youtu\.be)\S+?v=\K\S+/i", $user_markdown)) {
-                echo "Woohoo";
-                // return open youtube player
+                $entire_link = "/(?=https:\/\/www\.(?:youtube\.com|youtu\.be)\/watch\?v=([a-zA-Z0-9\_]{11})&list=([a-zA-Z]{5})-([a-zA-Z]{9})&index=([0-9]+))(?<!([a-zA-Z]))/i";
+                $search = "/(?:youtube\.com|youtu\.be)\/watch\?v=([a-zA-Z0-9]+)/i";
+                $replace = '<center><iframe width="560" height="315" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe></center>';
+
+                $iPlayer = preg_replace($search, $replace, $entire_link);
+
+                echo $iPlayer['page_content'];
+                // ^^returns content of this array
             }
         }
 
