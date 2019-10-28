a76000ce2fdaa737bd76469ca4d6d7bbd690ff0e
bona-kim <bona.kim@outlook.be>
2019-10-28 13:52:56 +0100

    remove submit button(not necessary in the future)

--- src/Form/LanguageSwitcherType.php
+++ src/Form/LanguageSwitcherType.php
@@ -13,17 +13,13 @@ class LanguageSwitcherType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
         $builder
             ->add('language', EntityType::class, [ // build a select drop-down containing all of the entities in that Class
             'class' => Language::class,             // that class is the Language entities
 
             'choice_label' => 'name', // property that should be used for displaying the entities
             'choice_value' => 'code'  // give the langcode as a value
-
         ]);
-        $builder->add('Send', SubmitType::class);
-
     }
 
     public function configureOptions(OptionsResolver $resolver)
