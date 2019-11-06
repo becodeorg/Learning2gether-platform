9617e93e68a7551361cf1338a3fa6e6987b043f5
bona-kim <bona.kim@outlook.be>
2019-11-06 17:04:06 +0100

    Remove todo comment

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/{_locale}/login", name="app_login") //TODO ASK needed?
+     * @Route("/{_locale}/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
