5961f28550fcb0fd6f92dd36216f58f7225f0a88
Timmeahj <broostim@hotmail.be>
2019-12-05 11:52:40 +0100

    migration post trouble fixed

--- src/Migrations/Version20191204101045.php
+++ src/Migrations/Version20191204101045.php
@@ -22,11 +22,11 @@ final class Version20191204101045 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D1F55203D');
-        $this->addSql('DROP INDEX IDX_5A8A6C8D1F55203D ON post');
-        $this->addSql('ALTER TABLE post CHANGE topic_id question_id INT NOT NULL');
-        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
-        $this->addSql('CREATE INDEX IDX_5A8A6C8D1E27F6BF ON post (question_id)');
+        //$this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D1F55203D');
+        //$this->addSql('DROP INDEX IDX_5A8A6C8D1F55203D ON post');
+        //$this->addSql('ALTER TABLE post CHANGE topic_id question_id INT NOT NULL');
+        //$this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
+        //$this->addSql('CREATE INDEX IDX_5A8A6C8D1E27F6BF ON post (question_id)');
     }
 
     public function down(Schema $schema) : void
