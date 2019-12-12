d86bd3226151e061ef29469d52c6c7db76f60218
Nicnicsai <nicolereyesgg@hotmail.com>
2019-12-12 12:00:24 +0100

    Nicole's last update

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -18,7 +18,7 @@ class PostType  extends AbstractType
         $builder
             ->add('subjectPost', TextType::class)
             ->add('question_id', HiddenType::class)
-            ->add('postPost', SubmitType::class, array('label' => 'Post'));
+            ->add('postPost', SubmitType::class, array('label' => 'Reply'));
 
         $builder->setMethod('POST');
 
