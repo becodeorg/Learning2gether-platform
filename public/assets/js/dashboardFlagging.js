a46693ed30104e0da075f7861f40ee10a376f728
Alexandre Plennevaux <alexandre@pixeline.be>
2020-03-10 11:43:12 +0000

    tentative patch for js routing

--- public/assets/js/dashboardFlagging.js
+++ public/assets/js/dashboardFlagging.js
@@ -1,6 +1,12 @@
+const routes = require('./fos_js_routes.json');
+import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+
+Routing.setRoutingData(routes);
+Routing.generate('rep_log_list');
+
 function checkModule(moduleId) {
     let overlay = document.getElementById('overlay');
-    let url = Routing.generate('api_module', {module: moduleId}, true);
+    let url = Routing.generate('api_module', { module: moduleId }, true);
     let btn = document.getElementById('flagbtn-' + moduleId);
     let dump = document.getElementById('dump-' + moduleId);
     let publish = document.getElementById('publish-' + moduleId);
@@ -13,26 +19,26 @@ function checkModule(moduleId) {
         .then(function (response) {
             return response.json();
         }).then(function (data) {
-        if (data.length){
-            btn.classList.remove('spinner-border','spinner-border-sm');
-            btn.classList.add('badge', 'badge-danger');
-            btn.innerText = data.length + ' problems found!';
-            dump.style.display = 'inline';
-            overlay.style.display='block';
-            let listDump = '<ul style="width: 100%">';
-            for (let i = 0; i < data.length; i++){
-                listDump += '<li>' + data[i] + '.&nbsp</li>';
+            if (data.length) {
+                btn.classList.remove('spinner-border', 'spinner-border-sm');
+                btn.classList.add('badge', 'badge-danger');
+                btn.innerText = data.length + ' problems found!';
+                dump.style.display = 'inline';
+                overlay.style.display = 'block';
+                let listDump = '<ul style="width: 100%">';
+                for (let i = 0; i < data.length; i++) {
+                    listDump += '<li>' + data[i] + '.&nbsp</li>';
+                }
+                listDump += '</ul>';
+                dump.innerHTML = listDump;
+            } else {
+                btn.classList.remove('spinner-border', 'spinner-border-sm');
+                btn.classList.add('badge', 'badge-primary');
+                btn.innerText = 'No errors found';
+                let publishUrl = Routing.generate('publish_module', { module: moduleId }, true);
+                publish.innerHTML = '<a id="publishLink" class="flagbtn badge badge-success">Click here to publish!</a>';
+                let publishLink = document.getElementById('publishLink');
+                publishLink.setAttribute('href', publishUrl);
             }
-            listDump += '</ul>';
-            dump.innerHTML = listDump;
-        } else {
-            btn.classList.remove('spinner-border', 'spinner-border-sm');
-            btn.classList.add('badge', 'badge-primary');
-            btn.innerText = 'No errors found';
-            let publishUrl = Routing.generate('publish_module', {module: moduleId}, true);
-            publish.innerHTML = '<a id="publishLink" class="flagbtn badge badge-success">Click here to publish!</a>';
-            let publishLink = document.getElementById('publishLink');
-            publishLink.setAttribute('href', publishUrl);
-        }
-    });
+        });
 }
